5648b1d84f9ac70c13f793451dffde6adc306968
ashwaniagarwalhousinganywhere <93709274+ashwaniagarwalhousinganywhere@users.noreply.github.com>
2022-12-06 16:51:31 +0530

    VueTemplate - Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,88 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+
+  <button @click="changeName('namteste')">Change Name</button>
+
+  <h5>{{name}}</h5>
+
+  <PersonList :persons="person" @childClicked="ClickEventfired($event)"/>
+
+  <div>{{testMethod}}</div>
+
+  
+</template>
+
+<script lang="ts">
+
+import { defineComponent, ref } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+import Person from './Types/Person';
+import PersonList from './components/PersonList.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld,
+    PersonList
+  },
+
+  data()
+  {
+    return{
+       testName:'abc'
+    }
+  },
+  computed:{
+    testMethod()
+    {
+      return this.testName;
+    }
+  },
+setup(){
+
+  const person=ref<Person[]>([
+    {
+      id:1,
+      name:'name1',
+      address:'address1',
+      isActive:false
+    },
+    {
+      id:2,
+      name:'name2',
+      address:'address2',
+      isActive:true
+    },
+  ])
+  const name=ref('test1');
+  return {name,person};
+},
+  methods:{
+    changeName(name:string)
+    {
+     
+      this.name=name;
+    },
+      
+    ClickEventfired(name: string)
+    {
+      this.name=name;
+    }
+      
+
+
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/PersonList.vue
@@ -0,0 +1,57 @@
+<template>
+        <ul>
+            <li v-for="item in persons" :key="item.id">
+                <h2>{{item.name}}</h2>
+                <h2>{{item.address}}</h2>
+            </li>
+        </ul>
+
+        <h1 v-bind:class="{display:show}" > class binding</h1>
+        <input v-model="message" />
+
+        <h2>{{message}}</h2>
+
+        <button @click="onClickButton">Child to parent</button>
+</template>
+
+
+
+<script lang="ts">
+import Person from '@/Types/Person'
+import { defineComponent, PropType } from 'vue'
+
+export default defineComponent({
+
+props:{
+    persons:{
+        required:true,
+        type:Array as PropType<Person[]>,
+    }
+},
+
+methods:
+{
+    onClickButton(){
+this.$emit('childClicked','value from child to parent');
+    }
+},
+
+data()
+{
+    return{
+        show:true,
+        message:'test message'
+    }
+},
+
+    setup() {
+        
+    },
+})
+</script>
+
+<style scoped>
+  .display{
+    background-color: blue;
+  }
+</style>
